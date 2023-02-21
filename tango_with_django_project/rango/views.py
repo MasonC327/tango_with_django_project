b0a55695baed2fcb2cdba1dd643f647c648cdeeb
Mason Cameron <mason02cameron@gmail.com>
2023-02-21 19:30:18 +0000

    Chapter 4 Complete

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -12,7 +12,8 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Mason.'}
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 
